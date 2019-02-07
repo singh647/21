69f5bc897bd608a53a843aa9468ce28e1981a3fe
visualstudio <visualstudio@230-PC21>
2019-02-07 15:03:38 -0500

    wqe

--- 21/Program.cs
+++ 21/Program.cs
@@ -1,15 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _21
+
+
+namespace peter
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
+            var a = new TestQuestion2();
+
+            a.PlayingWithForLoops();
+
         }
+
     }
+
+
+
+    class TestQuestion2
+
+    {
+
+        public int myFavoriteVariable = 0;
+
+
+
+        public void PlayingWithForLoops()
+
+        {
+
+            // write a For Loop to Add 10 Numbers
+
+            for (; MyMethod();)
+
+            {
+
+                if (myFavoriteVariable > 10)
+
+                {
+
+                    Console.WriteLine("i am so out of here!");
+
+                    break;
+
+                }
+
+                Console.WriteLine("oh no I have to go through this stupid loop again...");
+
+            }
+
+        }
+
+        public bool MyMethod()
+
+        {
+
+            myFavoriteVariable++;
+
+            return true;
+
+        }
+
+    }
+
 }
+
+
+
+
+Reply
+Forward
